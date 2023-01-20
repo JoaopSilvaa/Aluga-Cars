953f8d2f309677144bdcfc1498e05ed25244d6e5
João Antônio Pereira da Silva <94487432+JoaopSilvaa@users.noreply.github.com>
2023-01-20 14:50:43 -0300

    feat: :construction: desenvolvendo interface CalculateRent e implementando a classe Aluguel

---
+++ AlugaCars/src/main/java/org/example/Classes/Aluguel.java
@@ -0,0 +1,29 @@
+package org.example.Classes;
+
+import org.example.Interfaces.CalculateRent;
+
+public class Aluguel implements CalculateRent {
+    private final Vehicle vehicle;
+    private final Person client;
+    private final int days;
+
+    public Aluguel(Vehicle vehicle, Person client, int days) {
+        this.vehicle = vehicle;
+        this.client = client;
+        this.days = days;
+    }
+
+    @Override
+    public double calculate() {
+        if((client.type().equalsIgnoreCase("física") || client.type().equalsIgnoreCase("fisica"))
+        && days > 5) {
+            return vehicle.rent() * client.discount() * days;
+        } else if ((client.type().equalsIgnoreCase("jurídica") || client.type().equalsIgnoreCase("juridica"))
+                && days > 3) {
+            return vehicle.rent() * client.discount() * days;
+        }
+        else {
+            return vehicle.rent() * days;
+        }
+    }
+}
